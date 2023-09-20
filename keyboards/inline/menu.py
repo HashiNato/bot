9f2c1aac40897b204f9a61a5c8293ec5f5323f17
Aaroniuk <145377871+Aaroniuk@users.noreply.github.com>
2023-09-20 17:27:09 +0200

    Add files via upload

---
+++ keyboards/inline/menu.py
@@ -0,0 +1,9 @@
+from aingram type import InlineKeyboardMarkup, InlineKeyboardBotton
+first = InlineKeyboardMarkup(
+    inline_keyboard = [
+        InlineKeyboardBotton(text= "Botton", callback_data= "second" )])
+
+
+
+
+
