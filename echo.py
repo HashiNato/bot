7a1230362e48d62311bdec071fb06237a0b75236
Aaroniuk <145377871+Aaroniuk@users.noreply.github.com>
2023-09-23 16:27:43 +0200

    Add files via upload

--- echo.py
+++ echo.py
@@ -33,27 +33,17 @@ async def bot(message: types.Message):
     making_list.append(message.text)
     await message.answer("Виберіть собі ряд (A-H) і місце (1-15)")
 
-##
-        await message.answer(f'Чудово! Ось ваше місце: {making_list[2]}. Будемо чекати на вас!')
-        with open('making_file.txt', 'a') as f:
-            user_info = " ".join(making_list)
-            f.write(f'{user_info}\n')
-        making_list.clear()
-        await dp.current_state().reset_state()
-    else:
-        await message.answer("Похід на фільм відмінений. Якщо хочете записатися на інший день/час - скористайтеся командою /making")
-        making_list.clear()
-        await dp.current_state().reset_state()
+    await message.answer(f'Чудово! Ось ваше місце: {making_list[2]}. Будемо чекати на вас!')
+    with open('making_file.txt', 'a') as f:
+        user_info = " ".join(making_list)
+        f.write(f'{user_info}\n')
+    making_list.clear()
+    await dp.current_state().reset_state()
+    else await message.answer("Похід на фільм відмінений. Якщо хочете записатися на інший день/час - скористайтеся командою /making")
+    making_list.clear()
+    await dp.current_state().reset_state()
 
 @dp.message_handler (commands="tr")
 async def bot_echo(mesage: types.Message):
     await mesage.answer(f"Натисніть на кнопку", reply_markup=first)
 
-@dp.callback_query_handlers (test="bottonipressed")
-async def second_stop(call: types.CallbackQuery):
-    await call.message.answer("Done!!!!")
-
-#для echo
-@dp.message_handler(commands='exo')
-async def process_start_command(message: types.Message):
-    await message.reply("Хей гайс!", reply_markup=kb.greet_kb)
